f0d0c39504f62c1a4244cc9c7ff2cf81b933b621
Cassius <cassius.reynolds.dev@gmail.com>
2024-10-08 15:18:33 -0700

    added image to contact page, resized form for desktop, added alt text for profile image

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -8,17 +8,18 @@ const Contact = () => {
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
       </div>
       <div className="relative">
-        <h2 className="text-3xl font-bold pt-20 text-center text-gray-100">
+        <h2 className="text-5xl font-bold pt-20 text-center text-gray-100">
           Contact Me Today
         </h2>
-        <div className="flex flex-col md:flex-row justify-center items-center  py-10 gap-12">
-          <div className="rounded-md bg-gray-800 w-full md:w-auto p-8 text-gray-100 shadow-lg">
+        <div className="flex flex-col md:flex-row justify-center items-center   py-10 gap-12">
+      {/* contact info */}
+          <div className="rounded-md bg-gray-800 w-full md:w-auto p-8 h-[35rem] text-gray-100 shadow-lg">
             <p className="mb-4">
-              <span className="text-xl font-semibold">Cell:</span> (707)
+              <span className="text-2xl font-semibold">Cell:</span> (707)
               724-1815
             </p>
             <p className="mb-4">
-              <span className="text-xl font-semibold">Email:</span>{" "}
+              <span className="text-2xl font-semibold">Email:</span>{" "}
               <a
                 href="mailto:cassius.reynolds.dev@gmail.com"
                 className="text-blue-400 hover:underline"
@@ -27,7 +28,7 @@ const Contact = () => {
               </a>
             </p>
             <p className="mb-4">
-              <span className="text-xl font-semibold">LinkedIn:</span>{" "}
+              <span className="text-2xl font-semibold">LinkedIn:</span>{" "}
               <a
                 href=""
                 target="_blank"
@@ -38,7 +39,7 @@ const Contact = () => {
               </a>
             </p>
             <p>
-              <span className="text-xl font-semibold">GitHub:</span>{" "}
+              <span className="text-2xl font-semibold">GitHub:</span>{" "}
               <a
                 href="https://github.com/cassius2828"
                 target="_blank"
@@ -48,8 +49,10 @@ const Contact = () => {
                 github/cassius2828
               </a>
             </p>
+            <img className="w-80 mt-5" src="https://cdn.prod.website-files.com/5fd2ba952bcd68835f2c8254/654553fedbede7976b97eaf5_Professional-5.remini-enhanced.webp" alt="" />
           </div>
-          <form className="mt-4 w-full max-w-md bg-gray-800 p-8 rounded-md shadow-lg">
+          {/* form */}
+          <form className="mt-4 w-full max-w-lg h-[35rem] bg-gray-800 p-8 rounded-md shadow-lg">
             <div className="mb-4">
               <label className="block text-gray-200 mb-2">Name</label>
               <input
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,5 +1,5 @@
 // src/components/Hero.jsx
-import  { useEffect } from "react";
+import { useEffect } from "react";
 import { WavyBackground } from "../ui/wavy-background";
 import IconList from "../SocialMedia/IconList";
 import { FeaturedProjectCard } from "../Projects/FeaturedProjectCard";
@@ -8,8 +8,8 @@ import { getFeaturedProjects } from "../../service/projectsService";
 
 const Hero = () => {
   const { fetchProjects, featuredProjects, isLoading } = useGlobalContext();
-console.log(fetchProjects, ' <-- fetch projects func')
-console.log(featuredProjects, ' <-- featured projects')
+  console.log(fetchProjects, " <-- fetch projects func");
+  console.log(featuredProjects, " <-- featured projects");
   useEffect(() => {
     fetchProjects("setFeaturedProjects", getFeaturedProjects);
   }, []);
@@ -38,7 +38,7 @@ console.log(featuredProjects, ' <-- featured projects')
               <img
                 className=""
                 src="https://cdn.prod.website-files.com/5fd2ba952bcd68835f2c8254/654553fedbede7976b97eaf5_Professional-5.remini-enhanced.webp"
-                alt=""
+                alt="Cassius Reynolds Headshot"
               />
             </div>
           </div>
@@ -68,49 +68,9 @@ console.log(featuredProjects, ' <-- featured projects')
             })}
           </ul>
         </div>
-        {/* <div className=" w-1/2">
-          <p className="mt-4 text-3xl">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
-            autem temporibus eveniet quam? Aperiam blanditiis quas dicta
-            explicabo exercitationem quae, sunt quisquam velit consectetur,
-            consequuntur, quasi alias incidunt cupiditate esse.
-          </p>
-          <div className="space-x-4 mt-8">
-            <button className="border-2 border-blue-500 text-blue-500 text-3xl bg-transparent px-6 py-2 mt-4 rounded transition ease-in-out duration-300 transform hover:bg-blue-500 hover:text-[#f2f2f2] hover:-translate-y-1">
-              Projects
-            </button>
-            <button className="text-blue-500 text-3xl px-6 py-2 mt-4 rounded transition ease-in-out duration-300 transform hover:translate-x-1">
-              Contact
-            </button>
-          </div>
-        </div> */}
-        {/* <div className="w-1/3 rounded-xl overflow-hidden">
-      <img
-        className=""
-        src="https://cdn.prod.website-files.com/5fd2ba952bcd68835f2c8254/654553fedbede7976b97eaf5_Professional-5.remini-enhanced.webp"
-        alt=""
-      />
-    </div> */}
       </div>
     </section>
   );
 };
 
 export default Hero;
-
-/*
-
-     <p className="mt-4 text-3xl w-1/2">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
-              autem temporibus eveniet quam? Aperiam blanditiis quas dicta
-              explicabo exercitationem quae, sunt quisquam velit consectetur,
-              consequuntur, quasi alias incidunt cupiditate esse.
-            </p>
-            <div className="space-x-4 mt-8">
-              <button className="border-2 border-gray-100 text-gray-100 text-3xl bg-transparent px-6 py-2 mt-4 rounded transition ease-in-out duration-300 transform hover:bg-blue-500 hover:text-[#f2f2f2] hover:-translate-y-1">
-                Projects
-              </button>
-              <button className="text-gray-100 text-3xl px-6 py-2 mt-4 rounded transition ease-in-out duration-300 transform hover:translate-x-1">
-                Contact
-              </button>
-*/
