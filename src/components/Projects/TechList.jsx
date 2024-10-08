a3a8c8db5a49d43896bea6c177add324fc99b6bf
Cassius <cassius.reynolds.dev@gmail.com>
2024-10-08 15:57:27 -0700

    completed basic mobile responsiveness for landing page of site

--- src/components/Projects/TechList.jsx
+++ src/components/Projects/TechList.jsx
@@ -23,12 +23,12 @@ const devTechnologies = [
 
 export const TechList = ({ technologies=devTechnologies }) => {
   return (
-    <ul className="tech-list flex flex-wrap gap-4 my-8  p-2 justify-center items-start overflow-y-scroll text-2xl w-full  custom-scrollbar ">
+    <ul className="tech-list flex flex-wrap gap-4 my-8  p-2 justify-center items-start overflow-y-scroll text-xl md:text-2xl w-full  custom-scrollbar ">
       {technologies?.map((item) => {
         return (
           <li
             key={item.name + item.idx}
-            className="tech-list-item bg-blue-400 p-4 mx-4 rounded-3xl shadow-lg flex items-center justify-center w-40 h-16"
+            className="tech-list-item bg-blue-400 p-4 mx-4 rounded-3xl shadow-lg flex items-center justify-center w-32 md:w-40 h-16"
           >
             {item.name}
           </li>
