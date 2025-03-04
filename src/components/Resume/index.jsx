173ac2ff5e49a785e087cbe859087e5393f3bbdd
Cassius <cassius.reynolds.dev@gmail.com>
2025-03-04 10:01:34 -0800

    updated resume link to go to google drive instead of the current url

--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -2,16 +2,15 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FixedAlert } from "../Reuseables/Alert";
 const resumeS3Link = `https://5-06-sei.s3.us-west-1.amazonaws.com/portfolio/resume/Cassius_Reynolds_-_Software_Engineer.pdf`;
-
+const googleDriveResumeLink = `https://drive.google.com/file/d/1Q3qWKgDjpOQriR39708bYz8hhhPDNFno/view?usp=drive_link`
 const ResumePage = () => {
   const [isCopiedMessage, setIsCopiedMessage] = useState("");
 
   const navigate = useNavigate();
-  const url = encodeURI(window.location.href);
 
   const handleCopyLink = async () => {
     try {
-      await navigator.clipboard.writeText(url);
+      await navigator.clipboard.writeText(googleDriveResumeLink);
       setIsCopiedMessage("Copied Link");
 
       // Clear the message after 1 second
